3facf3fda774d2a0cc8e204a9a57fe0b608bb6f1
Johnny Peckover <johnny-peckover@live.co.uk>
2019-06-14 16:16:16 +0100

    added functionality to register team button

--- src/main/webapp/createTeam.js
+++ src/main/webapp/createTeam.js
@@ -1,9 +1,25 @@
+let team = {
+    name: "Team United",
+    homeColour: "blue",
+    awayColour: "green"
+}
+
+
 function createTeam() {
 
-    setTeamDetails();
+    if (document.getElementById("name").value != "") {
+        team.name = document.getElementById("name").value;
+    }
+    if (document.getElementById("homeColour").value != "") {
+        team.cost = document.getElementById("surnameInput").value;
+    }
+    if (document.getElementById("awayColour").value != "") {
+        team.type = document.getElementById("contactNoInput").value;
+    }
+
     let jsonTeam = JSON.stringify(team);
     console.log(jsonTeam);
-    serverRequest("POST", "/teams", jsonTeam).then((request) => {
+    serverRequest("POST", "/team", jsonTeam).then((request) => {
         console.log(request.responseText);
     })
 }
