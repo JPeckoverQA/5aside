3173f3bef7fbcbf0fecc6b272c558e9bfc42f06a
Johnny Peckover <johnny-peckover@live.co.uk>
2019-06-14 14:59:17 +0100

    login session storage

--- src/main/webapp/verifyPlayer.js
+++ src/main/webapp/verifyPlayer.js
@@ -1,12 +1,12 @@
 function verifyPlayer() {
 
     dataRequest("GET", "/player", null).then((request) => {
-        let allPlayers = JSON.parse(request.responseText);
+        const allPlayers = JSON.parse(request.responseText);
 
             for (let player of allPlayers) {
                 if (player.email === document.getElementById("emailLogin").value) {  //create input form for both
                     console.log('Player found', player);
-                    let user = player;
+                    const user = player;
                     sessionStorage.setItem('playerId', user.id);
                     sessionStorage.setItem('teamId', user.team);
                     window.location.href = 'http://35.246.42.33:8080/5aside-1.0/team/';
