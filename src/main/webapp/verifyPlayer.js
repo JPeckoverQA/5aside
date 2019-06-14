7bd188bb22f23d85c490d1444487a67ca516b1b6
Johnny Peckover <johnny-peckover@live.co.uk>
2019-06-14 13:37:16 +0100

    typo

--- src/main/webapp/verifyPlayer.js
+++ src/main/webapp/verifyPlayer.js
@@ -5,7 +5,7 @@ function verifyPlayer() {
         .then(allPlayers => {
             for (let player of allPlayers) {
                 if (player.email === document.getElementById("emailLogin").value) {  //create input form for both
-                    console.log('Player found', account);
+                    console.log('Player found', player);
                     return true;
                 }
             }
