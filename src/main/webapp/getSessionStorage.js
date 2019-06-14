0575efac7d2583e7a1afc10cba6cc7d16b99682f
Johnny Peckover <johnny-peckover@live.co.uk>
2019-06-14 17:48:06 +0100

    use session storage

---
+++ src/main/webapp/getSessionStorage.js
@@ -0,0 +1,10 @@
+
+const userTeamId = sessionStorage.getItem(teamId);
+
+function getUserTeam(userTeamId) {
+    serverRequest("GET", "/player/${userTeamId}", userTeamId).then((request) => {
+        let teamDetails = JSON.parse(request.responseText);
+        return teamDetails;
+        console.log(teamDetails);
+    })
+}
