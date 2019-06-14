a8a875fbf5beafa8d0cdb755d06a42a36683d0f0
Johnny Peckover <johnny-peckover@live.co.uk>
2019-06-14 22:46:55 +0100

    get team details from session storage corrections

--- src/main/webapp/getTeam.js
+++ src/main/webapp/getTeam.js
@@ -10,7 +10,7 @@ function getTeam(teamId) {
 function userTeamName(teamId) {
     dataRequest("GET", "/team/" + teamId).then((request) => {
         let teamDetails = JSON.parse(request.responseText);
-        document.getElementById(userTeamNameHeader).innerHTML(teamDetails);
+        document.getElementById(userTeamNameHeader).innerHTML(teamDetails.name);
 
     })
 }
