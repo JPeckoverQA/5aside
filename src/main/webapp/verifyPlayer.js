258aac2ce7bc75a0bc4ea2303b81f7728bea7b1e
Johnny Peckover <johnny-peckover@live.co.uk>
2019-06-14 13:58:32 +0100

    combined getAllPlayers and verifyPlayer

--- src/main/webapp/verifyPlayer.js
+++ src/main/webapp/verifyPlayer.js
@@ -1,8 +1,8 @@
 function verifyPlayer() {
-  
 
-    getAllPlayers()
-        .then(allPlayers => {
+    dataRequest("GET", "/player", null).then((request) => {
+        let allPlayers = JSON.parse(request.responseText);
+
             for (let player of allPlayers) {
                 if (player.email === document.getElementById("emailLogin").value) {  //create input form for both
                     console.log('Player found', player);
