8a35cc3179316b439b89b04ec77b197a81da0744
Johnny Peckover <johnny-peckover@live.co.uk>
2019-06-14 14:43:33 +0100

    login button redirect

--- src/main/webapp/verifyPlayer.js
+++ src/main/webapp/verifyPlayer.js
@@ -6,8 +6,7 @@ function verifyPlayer() {
             for (let player of allPlayers) {
                 if (player.email === document.getElementById("emailLogin").value) {  //create input form for both
                     console.log('Player found', player);
-                    return true;
-                    //window.location.href = 'http://35.246.42.33:8080/5aside-1.0/team/';
+                    window.location.href = 'http://35.246.42.33:8080/5aside-1.0/team/';
                 }
             }
             console.warn('No player recognised')
