1731c7577b5a24d79f2f9fe045c4474ac1f718bb
Johnny Peckover <johnny-peckover@live.co.uk>
2019-06-14 13:42:21 +0100

    typo

--- src/main/webapp/getAllPlayers.js
+++ src/main/webapp/getAllPlayers.js
@@ -1,5 +1,5 @@
 function getAllPlayers() {
-    serverRequest("GET", "/player", null).then((request) => {
+    dataRequest("GET", "/player", null).then((request) => {
         let allPlayers = JSON.parse(request.responseText);
         return allPlayers;
         console.log(allPlayers);
