1a3df0e7d8faded8a0e8178524e0599108cb22dc
Johnny Peckover <johnny-peckover@live.co.uk>
2019-06-14 11:52:05 +0100

    createPlayer function

--- src/main/webapp/createPlayer.js
+++ src/main/webapp/createPlayer.js
@@ -1,9 +1,27 @@
 function createPlayer() {
-   
-    setPlayerDetails();
-    let jsonPlayer = JSON.stringify(player);
-    console.log(jsonPlayer);
-    serverRequest("POST", "/players", jsonPlayer).then((request) => {
-        console.log(request.responseText);
-    })
+
+    defaultPlayer();
+
+    if (document.getElementById("forenameRegister").value != "") {
+        player.forename = document.getElementById("forenameInput").value;
+    }
+    if (document.getElementById("surnameRegister").value != "") {
+        player.surname = document.getElementById("surnameInput").value;
+    }
+    if (document.getElementById("contactNoRegister").value != "") {
+        player.contactNo = document.getElementById("contactNoInput").value;
+    }
+    if (document.getElementById("emailRegister").value != "") {
+        player.email = document.getElementById("emailInput").value;
+    }
+    if (document.getElementById("teamPinRegister").value != "") {
+        player.teamPin = document.getElementById("teamPin").value;
+    }
+}
+
+let jsonPlayer = JSON.stringify(player);
+console.log(jsonPlayer);
+serverRequest("POST", "/players", jsonPlayer).then((request) => {
+    console.log(request.responseText);
+})
 }
