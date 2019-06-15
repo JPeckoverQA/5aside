41023fdd66de7a1533ca006fa57796f3f1d374f2
Johnny Peckover <johnny-peckover@live.co.uk>
2019-06-15 22:05:01 +0100

    log out button typo correction

--- src/main/webapp/logOut.js
+++ src/main/webapp/logOut.js
@@ -1,5 +1,5 @@
 function logOut() {
-    window.location.href = '/5aside-1.0/player.html';
+    window.location.href = '/5aside-1.0';
     sessionStorage.clear('playerId');
     sessionStorage.clear('teamId');
 }
