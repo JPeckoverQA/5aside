730e85375c6d53fc6927e2ea7db3b1cac541d416
Johnny Peckover <johnny-peckover@live.co.uk>
2019-06-15 17:10:20 +0100

    auto fill form with current data

--- src/main/webapp/getPlayer.js
+++ src/main/webapp/getPlayer.js
@@ -12,11 +12,11 @@ function populateCurrentDetails(playerId) {
     dataRequest("GET", "/player/" + playerId).then((request) => {
         let playerDetails = JSON.parse(request.responseText);
 
-        document.getElementById('forenameUpdate').innerHTML = playerDetails.forename;
-        document.getElementById('surnameUpdate').innerHTML = playerDetails.surname;
-        document.getElementById('contactNoUpdate').innerHTML = playerDetails.contactNo;
-        document.getElementById('emailUpdate').innerHTML = playerDetails.email;
-        document.getElementById('teamPinUpdate').innerHTML = playerDetails.teamPin;
+        document.getElementById('forenameUpdate').value = playerDetails.forename;
+        document.getElementById('surnameUpdate').value = playerDetails.surname;
+        document.getElementById('contactNoUpdate').value = playerDetails.contactNo;
+        document.getElementById('emailUpdate').value = playerDetails.email;
+        document.getElementById('teamPinUpdate').value = playerDetails.teamPin;
 
     })
 }
