2e04e17e849fa32d9d2d9c3c4a3016b1e050d020
Johnny Peckover <johnny-peckover@live.co.uk>
2019-06-15 17:24:39 +0100

    update player data

--- src/main/webapp/getPlayer.js
+++ src/main/webapp/getPlayer.js
@@ -7,16 +7,3 @@ function getPlayer(playerId) {
         
     })
 }
-
-function populateCurrentDetails(playerId) {
-    dataRequest("GET", "/player/" + playerId).then((request) => {
-        let playerDetails = JSON.parse(request.responseText);
-
-        document.getElementById('forenameUpdate').value = playerDetails.forename;
-        document.getElementById('surnameUpdate').value = playerDetails.surname;
-        document.getElementById('contactNoUpdate').value = playerDetails.contactNo;
-        document.getElementById('emailUpdate').value = playerDetails.email;
-        document.getElementById('teamPinUpdate').value = playerDetails.teamPin;
-
-    })
-}
--- src/main/webapp/updatePlayer.js
+++ src/main/webapp/updatePlayer.js
@@ -1,19 +1,39 @@
-function updatePlayer() {
+function populateCurrentDetails(playerId) {
     dataRequest("GET", "/player/" + playerId).then((request) => {
         let playerDetails = JSON.parse(request.responseText);
 
+        document.getElementById('forenameUpdate').value = playerDetails.forename;
+        document.getElementById('surnameUpdate').value = playerDetails.surname;
+        document.getElementById('contactNoUpdate').value = playerDetails.contactNo;
+        document.getElementById('emailUpdate').value = playerDetails.email;
+        document.getElementById('teamPinUpdate').value = playerDetails.teamPin;
 
+    })
+}
 
+function updatePlayer(playerId) {
+    dataRequest("GET", "/player/" + playerId).then((request) => {
+        let player = JSON.parse(request.responseText)
 
+        if (document.getElementById("forenameUpdate").value != "") {
+            player.forename = document.getElementById("forenameUpdate").value;
+        }
+        if (document.getElementById("surnameUpdate").value != "") {
+            player.surname = document.getElementById("surnameUpdate").value;
+        }
+        if (document.getElementById("contactNoUpdate").value != "") {
+            player.contactNo = document.getElementById("contactNoUpdate").value;
+        }
+        if (document.getElementById("emailUpdate").value != "") {
+            player.email = document.getElementById("emailUpdate").value;
+        }
+        if (document.getElementById("teamPinUpdate").value != "") {
+            player.teamPin = document.getElementById("teamPinUpdate").value;
+        }
 
-
-
-    let playerId = document.getElementById("playerIdInput").value;    
-
-    setPlayerDetails();
     let playerJSON = JSON.stringify(player);
     console.log(jsonPlayer);
-    dataRequest("PUT", "/players/${playerId}", playerJSON).then((request) => {
+    dataRequest("PUT", "/players/" + playerId, playerJSON).then((request) => {
         console.log(request.responseText);
     })
 }
