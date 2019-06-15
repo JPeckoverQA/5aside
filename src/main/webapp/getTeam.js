caf40059c60e85c3647f5a8bdb2806b9d62e0fa8
Johnny Peckover <johnny-peckover@live.co.uk>
2019-06-15 13:32:14 +0100

    team kit colours

--- src/main/webapp/getTeam.js
+++ src/main/webapp/getTeam.js
@@ -3,9 +3,13 @@ function getTeam(teamId) {
         let teamDetails = JSON.parse(request.responseText);
         
         let userTeamName = teamDetails.name;
-        console.log(userTeamName);
+        let teamHomeColour = teamDetails.homeColour;
+        let teamAwayColour = teamDetails.awayColour;
+
         document.getElementById('userTeamNameHeader').innerHTML = userTeamName;
         document.getElementById('teamPageTitle').innerHTML = userTeamName;
+        document.getElementById('teamHomeColour').innerHTML = "Home kit: " + teamHomeColour;
+        document.getElementById('teamAwayColour').innerHTML = "Away kit: " + teamAwayColour;
         return teamDetails;
         
     })
