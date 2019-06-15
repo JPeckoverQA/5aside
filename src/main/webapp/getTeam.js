2a1a6d500f20c47535595444d2126eb0a4907e41
Johnny Peckover <johnny-peckover@live.co.uk>
2019-06-15 21:17:44 +0100

    Team profile - server error corrected - typo

--- src/main/webapp/getTeam.js
+++ src/main/webapp/getTeam.js
@@ -11,3 +11,10 @@ function displayTeamDetails(teamId) {
         
     })
 }
+
+function displayTeamName(teamId) {
+    dataRequest("GET", "/team/" + teamId).then((request) => {
+        let teamDetails = JSON.parse(request.responseText);
+        document.getElementById('userTeamNameHeader').innerHTML = teamDetails.name;
+    })
+}
