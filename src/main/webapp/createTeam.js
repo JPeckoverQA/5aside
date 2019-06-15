e0cec4c07e9fae6c605d4b357015fcec09959ecf
Johnny Peckover <johnny-peckover@live.co.uk>
2019-06-15 22:22:43 +0100

    register team form, must enter text

--- src/main/webapp/createTeam.js
+++ src/main/webapp/createTeam.js
@@ -1,20 +1,19 @@
-let team = {
-    name: "Team United",
-    homeColour: "blue",
-    awayColour: "white"
-}
-
-
 function createTeam() {
 
     if (document.getElementById("teamNameRegister").value != "") {
         team.name = document.getElementById("teamNameRegister").value;
+    } else {
+        window.alert("You must enter a team name.")
     }
     if (document.getElementById("homeKitColourRegister").value != "") {
         team.homeColour = document.getElementById("homeKitColourRegister").value;
+    } else {
+        window.alert("You must enter a home kit colour.")
     }
     if (document.getElementById("awayKitColourRegister").value != "") {
         team.awayColour = document.getElementById("awayKitColourRegister").value;
+    } else {
+        window.alert("You must enter an away kit colour.")
     }
 
     let jsonTeam = JSON.stringify(team);
