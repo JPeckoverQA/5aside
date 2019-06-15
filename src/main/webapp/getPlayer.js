d72d52b30e963f112f21b1dd1d6939fcffc38685
Johnny Peckover <johnny-peckover@live.co.uk>
2019-06-15 16:50:27 +0100

    auto fill form with current data

--- src/main/webapp/getPlayer.js
+++ src/main/webapp/getPlayer.js
@@ -3,6 +3,7 @@ function getPlayer(playerId) {
         let playerDetails = JSON.parse(request.responseText);
 
         document.getElementById('userForenameHeader').innerHTML = "Welcome " + playerDetails.forename + "!";
+        document.getElementById('forenameUpdate').innerHTML = playerDetails.forename;
         return playerDetails;
         
     })
