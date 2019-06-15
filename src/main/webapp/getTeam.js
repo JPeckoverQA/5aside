81a20099d6bf0758abe080d4a835ce71ea758bda
Johnny Peckover <johnny-peckover@live.co.uk>
2019-06-15 16:25:10 +0100

    typo

--- src/main/webapp/getTeam.js
+++ src/main/webapp/getTeam.js
@@ -2,7 +2,7 @@ function getTeam(teamId) {
     dataRequest("GET", "/team/" + teamId).then((request) => {
         let teamDetails = JSON.parse(request.responseText);
         
-        document.getElementById('userTeamNameHeader').innerHTML = ;
+        document.getElementById('userTeamNameHeader').innerHTML = teamDetails.name;
         document.getElementById('teamPageTitle').innerHTML = teamDetails.name;
         document.getElementById('teamHomeColour').innerHTML = "Home kit: " + teamDetails.homeColour;
         document.getElementById('teamAwayColour').innerHTML = "Away kit: " + teamDetails.awayColour;
