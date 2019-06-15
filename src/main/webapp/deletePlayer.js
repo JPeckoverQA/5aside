cce2ce2d0010ce78edf40315e0d0bc219fdc86f7
Johnny Peckover <johnny-peckover@live.co.uk>
2019-06-15 18:15:27 +0100

    delete user with prompt typo 2

--- src/main/webapp/deletePlayer.js
+++ src/main/webapp/deletePlayer.js
@@ -2,7 +2,7 @@ function deletePlayer(playerId) {
     let deleteConfirm = confirm("Are you sure you want to delete your account?");
     if (deleteConfirm == true) {
         let playerIdJSON = JSON.stringify(playerId);
-        dataRequest("DELETE", "/players/" + playerId, playerIdJSON);
+        dataRequest("DELETE", "/player/" + playerId, playerIdJSON);
     }
 }
 
