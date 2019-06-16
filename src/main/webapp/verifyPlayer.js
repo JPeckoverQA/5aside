91eb110b3e5e9fba7c0c7e9bdde8601d57d0d690
Johnny Peckover <johnny-peckover@live.co.uk>
2019-06-16 15:43:57 +0100

    session storage fix

--- src/main/webapp/verifyPlayer.js
+++ src/main/webapp/verifyPlayer.js
@@ -6,6 +6,7 @@ function verifyPlayer(userEmail) {
     console.log(userEmail);
     verificationRequest("GET", "/playerEmail/" + userEmail).then((request) => {
         let user = JSON.parse(request.responseText);
+        console.log(user);
 
         sessionStorage.setItem('playerId', user.id);
         sessionStorage.setItem('teamId', user.teamPin);
@@ -13,3 +14,26 @@ function verifyPlayer(userEmail) {
         return;
     })
 }
+
+
+//function verifyPlayer() {
+
+//    dataRequest("GET", "/player", null).then((request) => {
+//        const allPlayers = JSON.parse(request.responseText);
+
+//        for (let player of allPlayers) {
+//            if (player.email === document.getElementById("emailLogin").value) {
+//                console.log('Player found', player);
+//                const user = player;
+//                console.log(user);
+//                sessionStorage.setItem('playerId', user.id);
+//                sessionStorage.setItem('teamId', user.teamPin);
+//                window.location.href = '/5aside-1.0/team.html';
+//                return;
+
+//            }
+//        }
+//        window.alert('No player recognised')
+//        return false;
+//    });
+//};
