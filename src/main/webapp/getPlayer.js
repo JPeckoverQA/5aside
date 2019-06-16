e78cc4bbde3367742f60529638cbaba97ca49dd2
Johnny Peckover <johnny-peckover@live.co.uk>
2019-06-16 12:22:11 +0100

    session storage correction2

--- src/main/webapp/getPlayer.js
+++ src/main/webapp/getPlayer.js
@@ -2,8 +2,6 @@ function welcomePlayer(playerId) {
     dataRequest("GET", "/player/" + playerId).then((request) => {
         let playerDetails = JSON.parse(request.responseText);
 
-        document.getElementById('userForenameHeader').innerHTML = "Hello " + playerDetails.forename + "!";
-        return playerDetails;
-        
+        document.getElementById('userForenameHeader').innerHTML = "Hello " + playerDetails.forename + "!";       
     })
 }
