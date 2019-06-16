9e65eeee6d014fd5f1978bd97d72f7da9fbcda9b
Johnny Peckover <johnny-peckover@live.co.uk>
2019-06-16 12:13:09 +0100

    session storage correction

--- src/main/webapp/verifyPlayer.js
+++ src/main/webapp/verifyPlayer.js
@@ -1,6 +1,8 @@
 function verifyPlayer(userEmail) {
 
     verificationRequest("GET", "/player/" + userEmail).then((request) => {
+        let user = JSON.parse(request.responseText);
+
         sessionStorage.setItem('playerId', user.id);
         sessionStorage.setItem('teamId', user.teamPin);
         window.location.href = '/5aside-1.0/team.html';
