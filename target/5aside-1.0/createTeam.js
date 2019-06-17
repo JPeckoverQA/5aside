ea18cdfc50205847e0d73201cba8014de9c711d0
Johnny <johnny-peckover@live.co.uk>
2019-06-17 15:13:38 +0100

    mvn reinstall

--- target/5aside-1.0/createTeam.js
+++ target/5aside-1.0/createTeam.js
@@ -30,8 +30,8 @@ function createTeam() {
     console.log(jsonTeam);
     dataRequest("POST", "/team", jsonTeam).then((request) => {
         console.log(request.responseText);
-
-        dataRequest("GET", "/teamName/" + document.getElementById("teamNameRegister")).then((request) => {
+        let inputTeamName = document.getElementById("teamNameRegister").value
+        dataRequest("GET", "/teamName/" + inputTeamName).then((request) => {
             let teamArray = JSON.parse(request.responseText);
 
             //reduces array of team objects to individual team objects. key is team name so pin can be found using team name
@@ -41,13 +41,13 @@ function createTeam() {
                     return teamObj;
                 }, {})
             const teamObject = arrayToObject(teamArray);
-            console.log(teamObject[name]);
-            console.log(teamObject[name].id);
-            let teamPin = teamObject[name].id;
+            console.log(teamObject[inputTeamName]);
+            console.log(teamObject[inputTeamName].id);
+            let teamPin = teamObject[inputTeamName].id;
 
-            window.alert("Your team PIN is: " + teamPin + ". You must submit this in your player profile, or when you register");
+            window.alert("Your team PIN is: " + teamPin + ".\n To register for " + inputTeamName +
+                " you must submit this PIN in your player profile, or during player registration");
            
-
             window.location.href = '/5aside-1.0';
 
         })
